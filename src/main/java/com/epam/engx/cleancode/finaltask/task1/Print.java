33b95f75f9c73e8187a4473e61c234ef5a2e30db
durgamani <durgamani_devarapu@epam.com>
2021-04-12 14:27:15 +0530

    Module 4 – Final Task

--- src/main/java/com/epam/engx/cleancode/finaltask/task1/Print.java
+++ src/main/java/com/epam/engx/cleancode/finaltask/task1/Print.java
@@ -1,231 +1,226 @@
 package com.epam.engx.cleancode.finaltask.task1;
 
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import com.epam.engx.cleancode.finaltask.task1.thirdpartyjar.Command;
 import com.epam.engx.cleancode.finaltask.task1.thirdpartyjar.DataSet;
-import com.epam.engx.cleancode.finaltask.task1.thirdpartyjar.View;
 import com.epam.engx.cleancode.finaltask.task1.thirdpartyjar.DatabaseManager;
-
-import java.util.List;
+import com.epam.engx.cleancode.finaltask.task1.thirdpartyjar.View;
 
 
 public class Print implements Command {
 
+    public static final String EQUAL_SIGN = "═";
+    public static final String SPACE = " ";
+    public static final int EVEN_LENGTH = 2;
+    public static final String TABLE_STRING = "║ Table '";
+    public static final String IS_EMPTY_OR_DOES_NOT_EXIST_MESSAGE = "' is empty or does not exist ║";
+    public static final String LINE_START_MSG = "╔";
+    public static final String LINE_END_STRING = "╗";
+    public static final String LAST_LINE_START_STRING = "╚";
+    public static final String LAST_LINE_END_STRING = "╝";
+    public static final String LINE_MIDDLE_STRING = "║";
+    public static final String MIDDLE_LINE_STRING = "╩";
+    public static final String SIDE_LINE_START_STRING = "╠";
+    public static final String MIDDLE_STRING = "╬";
+    public static final String SIDE_LINE_END_STRING = "╣";
+    public static final String MIDDLE_LAST_STRING = "╦";
+    public static final String NEW_LINE = "\n";
+    public static final String PRINT_MESSAGE = "print ";
     private View view;
-    private DatabaseManager manager;
+    private DatabaseManager databaseManager;
     private String tableName;
 
-    public Print(View view, DatabaseManager manager) {
+    public Print(View view, DatabaseManager databaseManager) {
         this.view = view;
-        this.manager = manager;
+        this.databaseManager = databaseManager;
     }
 
     public boolean canProcess(String command) {
-        return command.startsWith("print ");
+        return command.startsWith(PRINT_MESSAGE);
     }
 
     public void process(String input) {
-        String[] command = input.split(" ");
-        if (command.length != 2) {
-            throw new IllegalArgumentException("incorrect number of parameters. Expected 1, but is " + (command.length - 1));
-        }
+        String[] command = input.split(SPACE);
+        isValidCommand(command);
         tableName = command[1];
-        List<DataSet> data = manager.getTableData(tableName);
-        view.write(getTableString(data));
+        List<DataSet> data = databaseManager.getTableData(tableName);
+        view.write(convertTableDataToString(data));
     }
 
-    private String getTableString(List<DataSet> data) {
+    private void isValidCommand(String[] command) {
+        if (command.length != EVEN_LENGTH) {
+            throw new IllegalArgumentException("incorrect number of parameters. Expected 1, but is " + (command.length - 1));
+        }
+    }
+
+    private String convertTableDataToString(List<DataSet> data) {
         int maxColumnSize;
         maxColumnSize = getMaxColumnSize(data);
-        if (maxColumnSize == 0) {
-            return getEmptyTable(tableName);
-        } else {
-            return getHeaderOfTheTable(data) + getStringTableData(data);
-        }
+        return maxColumnSize == 0 ? getEmptyTable(tableName) : (createHeaderForTable(data) + convertTableData(data));
     }
 
     private String getEmptyTable(String tableName) {
-        String textEmptyTable = "║ Table '" + tableName + "' is empty or does not exist ║";
-        String result = "╔";
-        for (int i = 0; i < textEmptyTable.length() - 2; i++) {
-            result += "═";
-        }
-        result += "╗\n";
-        result += textEmptyTable + "\n";
-        result += "╚";
-        for (int i = 0; i < textEmptyTable.length() - 2; i++) {
-            result += "═";
-        }
-        result += "╝\n";
+        String textEmptyTable = TABLE_STRING + tableName + IS_EMPTY_OR_DOES_NOT_EXIST_MESSAGE;
+        String result = LINE_START_MSG;
+        int length = textEmptyTable.length() - EVEN_LENGTH;
+        result = calculateColumn(result, length, EQUAL_SIGN);
+        result += LINE_END_STRING + NEW_LINE;
+        result += textEmptyTable + NEW_LINE;
+        result += LAST_LINE_START_STRING;
+        result = calculateColumn(result, length, EQUAL_SIGN);
+        result += LAST_LINE_END_STRING + NEW_LINE;
         return result;
     }
 
     private int getMaxColumnSize(List<DataSet> dataSets) {
         int maxLength = 0;
-        if (dataSets.size() > 0) {
+
+        if (!dataSets.isEmpty()) {
             List<String> columnNames = dataSets.get(0).getColumnNames();
-            for (String columnName : columnNames) {
-                if (columnName.length() > maxLength) {
-                    maxLength = columnName.length();
-                }
-            }
+            maxLength = Collections.max(columnNames).length();
             for (DataSet dataSet : dataSets) {
-                List<Object> values = dataSet.getValues();
-                for (Object value : values) {
-//                    if (value instanceof String)
-                        if (String.valueOf(value).length() > maxLength) {
-                            maxLength = String.valueOf(value).length();
-                        }
-                }
+                List<Object> dataSetValues = dataSet.getValues();
+                Object dataSetValue  = getMaxDataSetValue(dataSetValues);
+                int maxValue = String.valueOf(dataSetValue).length();
+                maxLength = getMaxColumnSize(maxValue>maxLength, maxValue, maxLength);
             }
         }
         return maxLength;
     }
 
-    private String getStringTableData(List<DataSet> dataSets) {
+    private Object getMaxDataSetValue(List<Object> values) {
+        return Collections.max(values, new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return String.valueOf(o1).length() - String.valueOf(o2).length();
+            }
+        });
+    }
+
+    private String convertTableData(List<DataSet> dataSets) {
         int rowsCount;
         rowsCount = dataSets.size();
         int maxColumnSize = getMaxColumnSize(dataSets);
         String result = "";
-        if (maxColumnSize % 2 == 0) {
-            maxColumnSize += 2;
+        if (maxColumnSize % EVEN_LENGTH == 0) {
+            maxColumnSize += EVEN_LENGTH;
         } else {
             maxColumnSize += 3;
         }
         int columnCount = getColumnCount(dataSets);
         for (int row = 0; row < rowsCount; row++) {
             List<Object> values = dataSets.get(row).getValues();
-            result += "║";
+            result = result.concat(LINE_MIDDLE_STRING);
             for (int column = 0; column < columnCount; column++) {
                 int valuesLength = String.valueOf(values.get(column)).length();
-                if (valuesLength % 2 == 0) {
-                    for (int j = 0; j < (maxColumnSize - valuesLength) / 2; j++) {
-                        result += " ";
-                    }
-                    result += String.valueOf(values.get(column));
-                    for (int j = 0; j < (maxColumnSize - valuesLength) / 2; j++) {
-                        result += " ";
-                    }
-                    result += "║";
-                } else {
-                    for (int j = 0; j < (maxColumnSize - valuesLength) / 2; j++) {
-                        result += " ";
-                    }
-                    result += String.valueOf(values.get(column));
-                    for (int j = 0; j <= (maxColumnSize - valuesLength) / 2; j++) {
-                        result += " ";
-                    }
-                    result += "║";
-                }
+                result = calculateForColumns(maxColumnSize, result, valuesLength,
+                        String.valueOf(values.get(column)));
+                result= (valuesLength % EVEN_LENGTH == 0) ? result+LINE_MIDDLE_STRING : result+" "+LINE_MIDDLE_STRING;
             }
-            result += "\n";
+            result = result.concat(NEW_LINE);
             if (row < rowsCount - 1) {
-                result += "╠";
+                result = result.concat(SIDE_LINE_START_STRING);
                 for (int j = 1; j < columnCount; j++) {
-                    for (int i = 0; i < maxColumnSize; i++) {
-                        result += "═";
-                    }
-                    result += "╬";
+                    result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+                    result = result.concat(MIDDLE_STRING);
                 }
-                for (int i = 0; i < maxColumnSize; i++) {
-                    result += "═";
-                }
-                result += "╣\n";
+                result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+                result = result.concat(SIDE_LINE_END_STRING + NEW_LINE);
             }
         }
-        result += "╚";
+        result = result.concat(LAST_LINE_START_STRING);
         for (int j = 1; j < columnCount; j++) {
-            for (int i = 0; i < maxColumnSize; i++) {
-                result += "═";
-            }
-            result += "╩";
+            result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+            result = result.concat(MIDDLE_LINE_STRING);
         }
-        for (int i = 0; i < maxColumnSize; i++) {
-            result += "═";
+        result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+        result = result.concat(LAST_LINE_END_STRING + NEW_LINE);
+        return result;
+    }
+
+    private String calculateForColumns(int maxColumnSize, String result, int valuesLength, String s) {
+        result = calculateColumn(result, (maxColumnSize - valuesLength) / EVEN_LENGTH, SPACE);
+        result += s;
+        result = calculateColumn(result, (maxColumnSize - valuesLength) / EVEN_LENGTH, SPACE);
+        return result;
+    }
+
+    private String calculateColumn(String result, int i2, String s) {
+        for (int j = 0; j < i2; j++) {
+            result = result.concat(s);
         }
-        result += "╝\n";
         return result;
     }
 
     private int getColumnCount(List<DataSet> dataSets) {
         int result = 0;
-        if (dataSets.size() > 0) {
+        if (!dataSets.isEmpty()) {
             return dataSets.get(0).getColumnNames().size();
         }
         return result;
     }
 
-    private String getHeaderOfTheTable(List<DataSet> dataSets) {
+    private String createHeaderForTable(List<DataSet> dataSets) {
         int maxColumnSize = getMaxColumnSize(dataSets);
         String result = "";
         int columnCount = getColumnCount(dataSets);
-        if (maxColumnSize % 2 == 0) {
-            maxColumnSize += 2;
-        } else {
-            maxColumnSize += 3;
-        }
-        result += "╔";
+        maxColumnSize = getMaxColumnSize(maxColumnSize % EVEN_LENGTH == 0, maxColumnSize + EVEN_LENGTH, maxColumnSize + 3);
+        result += LINE_START_MSG;
         for (int j = 1; j < columnCount; j++) {
-            for (int i = 0; i < maxColumnSize; i++) {
-                result += "═";
-            }
-            result += "╦";
+            result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+            result = result.concat(MIDDLE_LAST_STRING);
         }
-        for (int i = 0; i < maxColumnSize; i++) {
-            result += "═";
-        }
-        result += "╗\n";
+        result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+        result += LINE_END_STRING + NEW_LINE;
         List<String> columnNames = dataSets.get(0).getColumnNames();
         for (int column = 0; column < columnCount; column++) {
-            result += "║";
+            result = result.concat(LINE_MIDDLE_STRING);
             int columnNamesLength = columnNames.get(column).length();
-            if (columnNamesLength % 2 == 0) {
-                for (int j = 0; j < (maxColumnSize - columnNamesLength) / 2; j++) {
-                    result += " ";
-                }
-                result += columnNames.get(column);
-                for (int j = 0; j < (maxColumnSize - columnNamesLength) / 2; j++) {
-                    result += " ";
-                }
-            } else {
-                for (int j = 0; j < (maxColumnSize - columnNamesLength) / 2; j++) {
-                    result += " ";
-                }
-                result += columnNames.get(column);
-                for (int j = 0; j <= (maxColumnSize - columnNamesLength) / 2; j++) {
-                    result += " ";
-                }
-            }
+            result = (columnNamesLength % EVEN_LENGTH == 0) ? calculateForColumns(maxColumnSize, result, columnNamesLength,
+                    columnNames.get(column)) :
+                    calForOddColumnNames(maxColumnSize, result, columnNames, column, columnNamesLength);
         }
-        result += "║\n";
+        result = result.concat(LINE_MIDDLE_STRING+NEW_LINE);
 
         //last string of the header
-        if (dataSets.size() > 0) {
-            result += "╠";
-            for (int j = 1; j < columnCount; j++) {
-                for (int i = 0; i < maxColumnSize; i++) {
-                    result += "═";
-                }
-                result += "╬";
-            }
-            for (int i = 0; i < maxColumnSize; i++) {
-                result += "═";
-            }
-            result += "╣\n";
+        if (!dataSets.isEmpty()) {
+            result = calculateLastStringOfHeader(maxColumnSize, result, columnCount);
         } else {
-            result += "╚";
+            result = result.concat(LAST_LINE_START_STRING);
             for (int j = 1; j < columnCount; j++) {
-                for (int i = 0; i < maxColumnSize; i++) {
-                    result += "═";
-                }
-                result += "╩";
+                result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+                result = result.concat(MIDDLE_LINE_STRING);
             }
-            for (int i = 0; i < maxColumnSize; i++) {
-                result += "═";
-            }
-            result += "╝\n";
+            result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+            result = result.concat(LAST_LINE_END_STRING + NEW_LINE);
         }
+        return result;
+    }
+
+    private int getMaxColumnSize(boolean isValid, int i, int i2) {
+        return isValid ? i : i2;
+    }
+
+    private String calForOddColumnNames(int maxColumnSize, String result, List<String> columnNames, int column,
+                                        int columnNamesLength) {
+        result = calculateColumn(result, (maxColumnSize - columnNamesLength) / EVEN_LENGTH, SPACE);
+        result += columnNames.get(column);
+        result = calculateColumn(result, (maxColumnSize - columnNamesLength) / EVEN_LENGTH+1, SPACE);
+        return result;
+    }
+
+    private String calculateLastStringOfHeader(int maxColumnSize, String result, int columnCount) {
+        result = result.concat(SIDE_LINE_START_STRING);
+        for (int j = 1; j < columnCount; j++) {
+            result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+            result = result.concat(MIDDLE_STRING);
+        }
+        result = calculateColumn(result, maxColumnSize, EQUAL_SIGN);
+        result = result.concat(SIDE_LINE_END_STRING + NEW_LINE);
         return result;
     }
 }
